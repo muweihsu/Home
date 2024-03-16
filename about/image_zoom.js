b6f365ef936ffda5cf83bfbbfb301b39989924f7
muweihsu <143969493+muweihsu@users.noreply.github.com>
2024-03-17 04:35:07 +0800

    Site updated: 2024-03-17 04:35:06

--- about/image_zoom.js
+++ about/image_zoom.js
@@ -11,3 +11,13 @@ function toggleImageSize(imageElement) {
     imageElement.style.width = '400px';
   }
 }
+
+<!-- Import the CSS file -->
+<link rel="stylesheet" type="text/css" href="image_styles.css">
+
+<!-- Image thumbnail -->
+<div class="thumbnail-container">
+  <a href="#" onclick="toggleImageSize(this.children[0]); return false;">
+    <img src="path_to_your_image" alt="Image description" class="thumbnail-image" width="200" height="auto">
+  </a>
+</div>
