b2deeffd907ea5783783ed9de9c1b0e9413ef4d9
muweihsu <143969493+muweihsu@users.noreply.github.com>
2024-03-17 05:24:36 +0800

    Site updated: 2024-03-17 05:24:36

--- about/image_toggle.js
+++ about/image_toggle.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var images = document.querySelectorAll("img");
+  var images = document.querySelectorAll(".zoomable");
 
   images.forEach(function(img) {
-    img.style.height = "300px";
+    img.style.height = "100px";
     img.style.cursor = "pointer";
     img.onclick = function() {
-      if (img.style.height === "300px") {
-        img.style.height = "500px";
+      if (img.style.height === "100px") {
+        img.style.height = "400px";
       } else {
-        img.style.height = "300px";
+        img.style.height = "100px";
       }
     };
   });
