e3962a23adb3224b018ff4e24b13066b64587153
muweihsu <143969493+muweihsu@users.noreply.github.com>
2024-03-17 05:32:29 +0800

    Site updated: 2024-03-17 05:32:29

--- about/image_toggle.js
+++ about/image_toggle.js
@@ -2,14 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
   var images = document.querySelectorAll(".zoomable");
 
   images.forEach(function(img) {
-    img.style.height = "100px";
-    img.style.cursor = "pointer";
     img.onclick = function() {
-      if (img.style.height === "100px") {
-        img.style.height = "400px";
-      } else {
-        img.style.height = "100px";
-      }
+      img.classList.toggle("zoomed"); // ��?zoomed?���s�b
     };
   });
 });
